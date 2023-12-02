63c52ef1b3694c9046f69f01f280ef92046a8d49
adle951 <110898970+adle951@users.noreply.github.com>
2023-12-02 17:13:08 +0800

    add update data function

--- test_mongoDB.py
+++ test_mongoDB.py
@@ -28,13 +28,33 @@ users_collection = test_database.users
 # print(result.inserted_ids)
 
 # 查詢資料
-data = users_collection.find_one()
-print(data)
+# data = users_collection.find_one()
+# print(data)
+#
+# data2 = users_collection.find_one(ObjectId('6569ad8388e89abdc6cacac7'))
+# print(data2["nane"])
 
-data2 = users_collection.find_one(ObjectId('6569ad8388e89abdc6cacac7'))
-print(data2["nane"])
+data2 = users_collection.find_one({"name": "roey"})
+print(data2["gender"])
 
-data3 = users_collection.find()
-for _ in data3:
-    print(_["_id"])
-    print(_["gender"])
+# data3 = users_collection.find()
+# for _ in data3:
+#     print(_["_id"])
+#     print(_["gender"])
+
+# 修改一筆資料
+# result = users_collection.update_one({
+#     "gender" : "男"
+# }, { "$set" : { "gender" : "man"
+#     }
+# })
+
+# 修改多筆資料
+result = users_collection.update_many({
+    "gender" : "男"
+}, { "$set" : { "discription" : "add2"
+    }
+})
+
+print("符合條件的數量:", result.matched_count)
+print("實際更新的數量:", result.modified_count)
